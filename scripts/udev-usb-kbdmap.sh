ba9c352b690910b756b6c316a8d1c98d410dfc58
William Heinbockel <heinbockel@redhat.com>
2012-06-18 13:36:10 -0400

    Transition from xmodmap to xkbmap; add udev usb kb hotplug map support

---
+++ scripts/udev-usb-kbdmap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ `id -u` -ne 0 ]; then
+	echo "Script must be run with root permissions"
+	exit 1
+fi
+d="/etc/udev/rules.d"
+if [ -d "$d" ]; then
+	# Remap Caps Lock to ESC via udev to support USB Keyboard hotplugging
+	cat > "$d/99-usb-keyboard.rules" << EOF
+KERNEL=="event*", SUBSYSTEMS=="usb", RUN+="keymap $name 0x70039 esc"
+EOF
+fi
+
