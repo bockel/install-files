e80e6ee543ded123d44d21576650be2898e5cdf0
William Heinbockel <heinbockel@redhat.com>
2012-06-18 16:34:26 -0400

    Escape var name in udev script

--- scripts/udev-usb-kbdmap.sh
+++ scripts/udev-usb-kbdmap.sh
@@ -7,7 +7,7 @@ d="/etc/udev/rules.d"
 if [ -d "$d" ]; then
 	# Remap Caps Lock to ESC via udev to support USB Keyboard hotplugging
 	cat > "$d/99-usb-keyboard.rules" << EOF
-ACTION=="add", KERNEL=="event*", SUBSYSTEMS=="usb", RUN+="keymap $name 0x70039 esc"
+ACTION=="add", KERNEL=="event*", SUBSYSTEMS=="usb", RUN+="keymap \$name 0x70039 esc"
 EOF
 fi
 
