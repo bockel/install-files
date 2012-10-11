db3dcd8c4500df44f64ec046b6f7effa49109ac2
William Heinbockel <heinbockel@redhat.com>
2012-10-11 17:45:08 -0400

    Update vim to 72 tw, 3 ts sw; vim bundle submodule updates

---
+++ scripts/gnome3-config.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+gsettings set org.gnome.desktop.applications.terminal exec urxvtc
+gsettings set org.gnome.desktop.applications.terminal exec-arg "'-e'"
+
